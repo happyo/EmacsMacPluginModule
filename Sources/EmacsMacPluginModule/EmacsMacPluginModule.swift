1ef7f10785fff3ce2b6845303cf61296f2494843
happyo <xiaxiongzhi@corp.netease.com>
2024-06-08 22:50:35 +0800

    @dev # Finish cursor animation

--- Sources/EmacsMacPluginModule/EmacsMacPluginModule.swift
+++ Sources/EmacsMacPluginModule/EmacsMacPluginModule.swift
@@ -25,7 +25,6 @@ class EmacsMacPluginModule: Module {
     var env: Environment?
     var redView: NSView? = nil
     var animationTimer: Timer? = nil
-    var lastWindowInfo: WindowInfo? = nil
 
     func Init(_ env: Environment) throws {
         try env.defun("swift-create-window-info") { (env: Environment) -> WindowInfo in
@@ -75,10 +74,6 @@ class EmacsMacPluginModule: Module {
                 self.setupRedView(x: fixedX, y: fixedY, cursorWidth: model.width, cursorHeight: model.height)
             }
         }
-        
-        try env.defun("macos-module--clear-window-info") { (env: Environment) in
-            self.clearAnimations()
-        }
     }
 
     private func setupRedView(x: Int, y: Int, cursorWidth: Int, cursorHeight: Int) {
@@ -88,7 +83,6 @@ class EmacsMacPluginModule: Module {
         redView.layer?.backgroundColor = NSColor.red.cgColor
         view.addSubview(redView)
         self.redView = redView
-        self.lastWindowInfo = WindowInfo(x: x, y: y, width: cursorWidth, height: cursorHeight)
     }
 
     private func scheduleAnimation(toX x: Int, toY y: Int) {
@@ -100,29 +94,73 @@ class EmacsMacPluginModule: Module {
     
     private func performJellyAnimation(toX x: Int, toY y: Int) {
         guard let view = NSApp.mainWindow?.contentView, let redView = self.redView else { return }
-        let springAnimation = CASpringAnimation(keyPath: "position")
-        springAnimation.damping = 5.0 // Adjust damping to control the "bounciness"
-        springAnimation.stiffness = 100.0 // Adjust stiffness, higher values make the animation start faster
-        springAnimation.mass = 1.0 // Mass of the object, affecting the spring animation
-        springAnimation.initialVelocity = 0.0 // Initial velocity of the animation
-        springAnimation.fromValue = NSValue(point: redView.frame.origin)
-        springAnimation.toValue = NSValue(point: CGPoint(x: x, y: y))
-        springAnimation.duration = springAnimation.settlingDuration
-    
-        redView.layer?.add(springAnimation, forKey: "position")
-        redView.frame.origin = CGPoint(x: x, y: y) // Update final position to avoid snap-back
+        
+        // 获取当前起点和终点
+        let startPoint = redView.frame.origin
+        let endPoint = CGPoint(x: x, y: y)
+        let cursorSize = redView.frame.size
+        
+        // 创建一个三角形路径
+        let trianglePath = createTrianglePath(from: startPoint, to: endPoint, size: cursorSize)
+        
+        // 创建一个残影视图
+        let shadowLayer = CAShapeLayer()
+        shadowLayer.path = trianglePath.cgPath
+        shadowLayer.fillColor = redView.layer?.backgroundColor
+        shadowLayer.opacity = 0.5 // 初始不透明度为 0.5
+        view.layer?.addSublayer(shadowLayer)
+        
+        // 为红色视图添加移动动画
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = 0.1
+            context.timingFunction = CAMediaTimingFunction(name: .easeIn)
+            redView.animator().setFrameOrigin(endPoint)
+        }) {
+            shadowLayer.removeFromSuperlayer()
+        }
     }
 
-    func clearAnimations() {
-        animationTimer?.invalidate()
-        animationTimer = nil
-        redView?.layer?.removeAllAnimations()
-        redView?.removeFromSuperview()
-        redView = nil
+    // 创建三角形路径的辅助方法
+    private func createTrianglePath(from startPoint: CGPoint, to endPoint: CGPoint, size: CGSize) -> NSBezierPath {
+        let path = NSBezierPath()
+        path.move(to: startPoint)
+        
+        let topPoint = CGPoint(x: endPoint.x, y: endPoint.y + size.height)
+        let bottomPoint = CGPoint(x: endPoint.x, y: endPoint.y)
+        
+        path.line(to: topPoint)
+        path.line(to: bottomPoint)
+        path.close()
+        
+        return path
     }
+}
 
-    
-
+extension NSBezierPath {
+    var cgPath: CGPath {
+        let path = CGMutablePath()
+        var points = [CGPoint](repeating: .zero, count: 3)
+        
+        for i in 0..<elementCount {
+            let type = element(at: i, associatedPoints: &points)
+            switch type {
+            case .moveTo:
+                path.move(to: points[0])
+            case .lineTo:
+                path.addLine(to: points[0])
+            case .cubicCurveTo:
+                path.addCurve(to: points[2], control1: points[0], control2: points[1])
+            case .closePath:
+                path.closeSubpath()
+            case .quadraticCurveTo:
+                path.addQuadCurve(to: points[1], control: points[0])
+            @unknown default:
+                break
+            }
+        }
+        
+        return path
+    }
 }
 
 func createModule() -> Module { EmacsMacPluginModule() }
