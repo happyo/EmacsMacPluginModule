1166c46f8e1420473f3b9220bd7cff96756051be
happyo <xiaxiongzhi@corp.netease.com>
2024-06-11 11:29:39 +0800

    @dev # Refact code

---
+++ Sources/EmacsMacPluginModule/BasePlugin.swift
@@ -0,0 +1,11 @@
+//
+//  Created by belyenochi on 2024/06/11.
+//
+import AppKit
+import EmacsSwiftModule
+
+class BasePlugin {
+    public func initFunctions(_ env: Environment) throws {
+        
+    }
+}
